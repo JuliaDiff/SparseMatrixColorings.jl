026b715eb956eeab4c4b1354c98756f739fdba75
Alexis Montoison <Alexis.Montoison@amd.com>
2026-09-19 21:44:12 -0600

    Add an option decompression_uplo for symmetric results

--- src/interface.jl
+++ src/interface.jl
@@ -296,7 +296,7 @@ function _coloring(
     end
     color, star_set = argmin(maximum ∘ first, color_and_star_set_by_order)
     if speed_setting isa WithResult
-        return StarSetColoringResult(A, ag, color, star_set)
+        return StarSetColoringResult(A, ag, color, star_set, :F)
     else
         return color
     end
@@ -323,7 +323,7 @@ function _coloring(
         color, tree_set = argmin(maximum ∘ first, color_and_tree_set_by_order)
     end
     if speed_setting isa WithResult
-        return TreeSetColoringResult(A, ag, color, tree_set, R)
+        return TreeSetColoringResult(A, ag, color, tree_set, R, :F)
     else
         return color
     end
@@ -368,7 +368,7 @@ function _coloring(
         t -> maximum(t[3]) + maximum(t[4]), outputs_by_order
     )  # can't use ncolors without computing the full result
     if speed_setting isa WithResult
-        symmetric_result = StarSetColoringResult(A_and_Aᵀ, ag, color, star_set)
+        symmetric_result = StarSetColoringResult(A_and_Aᵀ, ag, color, star_set, :L)
         return BicoloringResult(
             A,
             ag,
@@ -418,7 +418,7 @@ function _coloring(
         t -> maximum(t[3]) + maximum(t[4]), outputs_by_order
     )  # can't use ncolors without computing the full result
     if speed_setting isa WithResult
-        symmetric_result = TreeSetColoringResult(A_and_Aᵀ, ag, color, tree_set, R)
+        symmetric_result = TreeSetColoringResult(A_and_Aᵀ, ag, color, tree_set, R, :L)
         return BicoloringResult(
             A,
             ag,
